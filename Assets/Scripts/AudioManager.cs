48b1c152681491397871414ec8f6b7b4f90de9dd
Georgi <Georgi.Kostov@fh-hagenberg.at>
2017-10-04 22:08:01 +0200

    innitial commit

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : AManager<AudioManager>
+{
+
+    //Audio Anim
+    public AudioSource audio;
+    public static float[] Samples = new float[512];
+    public static float[] FreqBand = new float[8];
+    public bool MicOn;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        if (MicOn)
+            StreamMic();
+    }
+
+    void Update()
+    {
+        audio.GetSpectrumData(Samples, 0, FFTWindow.Blackman);
+        MakeFrequencyBands();
+    }
+
+    void StreamMic()
+    {
+        audio.clip = Microphone.Start(Microphone.devices[0], true, 10, 44100);
+        audio.loop = true;
+        while (!(Microphone.GetPosition(null) > 0)) { }
+        audio.Play();
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += Samples[count] * (count + 1);
+                count++;
+            }
+            average /= count;
+            FreqBand[i] = average * 10;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Vals.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vals : AManager<Vals>
+{
+    public float CenterZ;
+    public float CenterY;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
